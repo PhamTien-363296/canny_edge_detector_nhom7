1d5339143476101ea9ffeb57ae8da3463967d0d9
vhminhthu <vhminhthu824@gmail.com>
2024-10-28 13:30:35 +0700

    nut thay doi chi so + camera b1

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
 import numpy as np
-from tkinter import filedialog as fd
+from tkinter import filedialog as fd, messagebox
 from scipy.signal import convolve2d
-
-photogaussian=None
 
 def main():
     main = tk.Tk()
     main.geometry('1000x600')
     main.resizable(False, False)
     main.title("Canny Edge Detector - NHÓM 7")
-    cap = cv2.VideoCapture(0)
+
+    default_kernel_size = 7
+    default_sigma = 1
+    kernel_size_var = tk.StringVar(value=str(default_kernel_size))
+    sigma_var = tk.StringVar(value=str(default_sigma))
+    kernel_size_var_cam = tk.StringVar(value=str(default_kernel_size))
+    sigma_var_cam = tk.StringVar(value=str(default_sigma))
 
     def show_frame(frame):
         frame.tkraise()
 
     def pickFileImage():
+        global img_tk, gray_np
         filetypes = (
             ('Image files', '*.jpg *.png *.jpeg *.gif'),
             ('All files', '*.*')
@@ -31,63 +36,118 @@ def main():
 
             gray = img.convert('L')
             gray_np = np.array(gray)
-            canny(gray_np)
+            canny_image()
 
             labelImage1_input.config(image=img_tk)
             labelImage1_input.image = img_tk
-
-            #Bước 1
-            labelImage1_input1.config(image=img_tk)
-            labelImage1_input1.image = img_tk
-            labelImage2_output1.config(image=photogaussian)
-            labelImage2_output1.image = photogaussian
-
-            #Bước 2
-            labelImage1_input2.config(image=photogaussian)
-            labelImage1_input2.image = photogaussian
-
-            #Bước 3
-            #labelImage1_input3.config(image=img_tk)
-            #labelImage1_input3.image = img_tk
-
-            #Bước 4
-            #labelImage1_input4.config(image=img_tk)
-            #labelImage1_input4.image = img_tk
 
             text1.delete('1.0', tk.END)
             text1.insert(tk.END, img_path)
 
     def showCamera():
-        imagecv2 = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(imagecv2)
+        cap = cv2.VideoCapture(0)
 
-        imgtk = ImageTk.PhotoImage(img)
-        labelCamera11.imgtk = imgtk
-        labelCamera11.configure(image=imgtk)
-        labelCamera11.after(20, showCamera)
+        if not cap.isOpened():
+            return
 
-    def canny(img):
-        #Bước 1
-        gaussian_output = gaussian_smoothing(img)
+        def update_frame():
+            ret, frame = cap.read()
+            if not ret:
+                return
+
+            global gray
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            edges = canny_camera(gray)
+            edges = np.uint8(edges)
+
+            cam = Image.fromarray(edges)
+            camtk = ImageTk.PhotoImage(cam)
+            labelCamera11.config(image=camtk)
+            labelCamera11.image = camtk
+
+            labelCamera11.after(10, update_frame)
+
+        update_frame()
+
+        def on_closing():
+            cap.release()
+            cv2.destroyAllWindows()
+            main.destroy()
+
+        main.protocol("WM_DELETE_WINDOW", on_closing)
+
+    def canny_camera(*args):
+        kernel_size = int(kernel_size_var_cam.get() or default_kernel_size)
+        sigma = float(sigma_var_cam.get() or default_sigma)
+
+        gaussian_output = gaussian_smoothing(gray, kernel_size=kernel_size, sigma=sigma)
+
+        return gaussian_output
+
+    def canny_image(*args):
+        # Bước 1:
+        kernel_size = int(kernel_size_var.get() or default_kernel_size)
+        sigma = float(sigma_var.get() or default_sigma)
+
+        gaussian_output = gaussian_smoothing(gray_np, kernel_size=kernel_size, sigma=sigma)
+        print('kernel_size: ' + str(kernel_size) + ' sigma: ' + str(sigma))
+
+        #---Xử lý hiển thị---
         gaussian_image = Image.fromarray(np.uint8(gaussian_output))
-        global photogaussian
         photogaussian = ImageTk.PhotoImage(gaussian_image)
 
-    #Bước 1: Lọc Gaussian làm mờ và mịn ảnh
-    def gaussian_smoothing(img, kernel_size=7, sigma=1):
+        #---Cập nhật hình ảnh trên giao diện--
+        labelImage1_input1.config(image=img_tk)
+        labelImage1_input1.image = img_tk
+        labelImage2_output1.config(image=photogaussian)
+        labelImage2_output1.image = photogaussian
+
+    #Bước 1:
+    def gaussian_smoothing(img, kernel_size, sigma):
         m, n = kernel_size, kernel_size
         gaussian_kernel = np.zeros((m, n))
         m = m // 2
         n = n // 2
         for x in range(-m, m + 1):
             for y in range(-n, n + 1):
-                x1 = sigma * (2 * np.pi)
+                x1 = (sigma ** 2) * (2 * np.pi)
                 x2 = np.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
                 gaussian_kernel[x + m, y + n] = (1 / x1) * x2
         gaussian_kernel /= np.sum(gaussian_kernel)
 
-        #thực hiện phép tích chập 2 chiều
         return convolve2d(img, gaussian_kernel, mode='same', boundary='symm')
+
+    #Theo dõi và gọi hàm
+    kernel_size_var.trace_add("write", canny_image)
+    sigma_var.trace_add("write", canny_image)
+
+    kernel_size_var_cam.trace_add("write", canny_camera)
+    sigma_var_cam.trace_add("write", canny_camera)
+
+    def luu_image():
+        kernel_size = int(kernel_size_var.get() or default_kernel_size)
+        sigma = float(sigma_var.get() or default_sigma)
+
+        print("kernel_size: ", kernel_size)
+        print("sigma1: ", sigma)
+
+        kernel_size_var.set(str(kernel_size))
+        sigma_var.set(str(sigma))
+
+        show_frame(frame3)
+
+    def luu_camera():
+        kernel_size = int(kernel_size_var_cam.get() or default_kernel_size)
+        sigma = float(sigma_var_cam.get() or default_sigma)
+
+        print("kernel_size: ", kernel_size)
+        print("sigma1: ", sigma)
+
+        kernel_size_var_cam.set(str(kernel_size))
+        sigma_var_cam.set(str(sigma))
+
+        show_frame(frame4)
 
     frame1 = tk.Frame(main, width=1000, height=600, bg='#f0f0f0')
     frame2 = tk.Frame(main, width=1000, height=600, bg='#f0f0f0')
@@ -97,8 +157,10 @@ def main():
     frame6 = tk.Frame(main, width=1000, height=600, bg='#f0f0f0')
     frame7 = tk.Frame(main, width=1000, height=600, bg='#f0f0f0')
     frame8 = tk.Frame(main, width=1000, height=600, bg='#f0f0f0')
+    frame9 = tk.Frame(main, width=1000, height=600, bg='#f0f0f0')
+    frame10 = tk.Frame(main, width=1000, height=600, bg='#f0f0f0')
 
-    for frame in (frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8):
+    for frame in (frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10):
         frame.grid(row=0, column=0, sticky='nsew')
 
     img = Image.open("background.png")
@@ -163,9 +225,19 @@ def main():
     image_icon = ImageTk.PhotoImage(imgimage_icon)
 
     text1 = tk.Text(frame3, width=30, height=1, font=('Arial', 17, 'bold'), fg="#007865", bg="white", selectbackground='#007865', wrap='none', relief='flat', padx=5, pady=5)
-    text1.place(relx=0.43, rely=0.2, anchor='center')
+    text1.place(relx=0.35, rely=0.2, anchor='center')
     button3 = tk.Button(frame3, text="Chọn file",image=image_icon, compound='left', command=lambda: pickFileImage(), bg='#007865', fg='white', font=('Arial', 16, 'bold'), relief='flat', borderwidth=0, padx=10)
-    button3.place(relx=0.7, rely=0.2, anchor='center')
+    button3.place(relx=0.62, rely=0.2, anchor='center')
+
+    def kiemtra_image():
+        if text1.get('1.0', tk.END).strip():
+            show_frame(frame9)
+        else:
+            messagebox.showinfo("Thông báo", "Vui lòng chọn ảnh trước khi thay đổi!")
+
+    button4 = tk.Button(frame3, text="Thay đổi chỉ số", command=kiemtra_image, bg='#007865', fg='#fff',
+                        font=('Arial', 13, 'bold'), relief='flat', borderwidth=0, padx=7)
+    button4.place(relx=0.77, rely=0.2, anchor='center')
 
     labelImage1 = tk.Label(frame3, text="Hình của bạn", font=('Arial', 16, 'bold'), bg='#c5d1dd', fg='#007865')
     labelImage1.place(relx=0.25, rely=0.35, anchor='center')
@@ -194,6 +266,10 @@ def main():
     camera_icon = ImageTk.PhotoImage(imgcamera_icon)
     button2 = tk.Button(frame4, text="Mở Camera", image=camera_icon, compound='left',command=lambda: showCamera(),  bg='#007865', fg='white', font=('Arial', 16, 'bold'), relief='flat', borderwidth=0, padx=10)
     button2.place(relx=0.5, rely=0.2, anchor='center')
+
+    button4 = tk.Button(frame4, text="Thay đổi chỉ số", command=lambda: show_frame(frame10), bg='#007865', fg='#fff',
+                        font=('Arial', 13, 'bold'), relief='flat', borderwidth=0, padx=7)
+    button4.place(relx=0.77, rely=0.2, anchor='center')
 
     labelCamera1 = tk.Label(frame4, text="Camera của bạn", font=('Arial', 16, 'bold'), bg='#c5d1dd', fg='#007865')
     labelCamera1.place(relx=0.26, rely=0.35, anchor='center')
@@ -278,8 +354,8 @@ def main():
 
     frameImage2 = tk.Frame(frame6, width=350, height=250, bg='#fff')
     frameImage2.place(relx=0.55, rely=0.53, anchor='nw')
-    labelImage22 = tk.Label(frameImage2)
-    labelImage22.place(relwidth=1, relheight=1)
+    labelImage2_output2 = tk.Label(frameImage2)
+    labelImage2_output2.place(relwidth=1, relheight=1)
 
     # Thiết kế Frame 7
     labelImg = tk.Label(frame7, image=photo)
@@ -324,8 +400,8 @@ def main():
                       fg='#007865')
     label1.place(relx=0.5, rely=0.08, anchor='center')
 
-    label1 = tk.Label(frame8, text="BƯỚC 4", font=('Arial', 20, 'bold'), bg='#CFE1E4', fg='#007865')
-    label1.place(relx=0.5, rely=0.15, anchor='center')
+    label2 = tk.Label(frame8, text="BƯỚC 4", font=('Arial', 20, 'bold'), bg='#CFE1E4', fg='#007865')
+    label2.place(relx=0.5, rely=0.15, anchor='center')
 
     button1 = tk.Button(frame8, text="Quay lại", command=lambda: show_frame(frame3), bg='#fff', fg='#007865',
                         font=('Arial', 10, 'normal'), width=10, height=1, relief='flat', borderwidth=0)
@@ -347,6 +423,57 @@ def main():
     frameImage2.place(relx=0.55, rely=0.53, anchor='nw')
     labelImage22 = tk.Label(frameImage2)
     labelImage22.place(relwidth=1, relheight=1)
+
+    # Thiết kế Frame 9
+    labelImg = tk.Label(frame9, image=photo)
+    labelImg.place(relwidth=1, relheight=1)
+
+    label1 = tk.Label(frame9, text="THÔNG TIN CHỈ SỐ", font=('Arial', 20, 'bold'), bg='#d0e7e5',
+                      fg='#007865')
+    label1.place(relx=0.5, rely=0.08, anchor='center')
+
+    label2 = tk.Label(frame9, text="Bước 1", font=('Arial', 20, 'bold'), bg='#CFE1E4', fg='#007865')
+    label2.place(relx=0.5, rely=0.15, anchor='center')
+
+    label1 = tk.Label(frame9, text="Kernel Size: ", font=('Arial', 15, 'bold'), bg='#D2D7E1', fg='#007865')
+    label1.place(relx=0.3, rely=0.22, anchor='center')
+    entry1 = tk.Entry(frame9,textvariable = kernel_size_var, font=('Arial',15,'normal'), fg='#007865', width=10, justify='center')
+    entry1.place(relx=0.425, rely=0.22, anchor='center')
+
+    label2 = tk.Label(frame9, text="Sigma: ", font=('Arial', 15, 'bold'), bg='#D2D7E1', fg='#007865')
+    label2.place(relx=0.6, rely=0.22, anchor='center')
+    entry2 = tk.Entry(frame9,textvariable = sigma_var, font=('Arial',15,'normal'), fg='#007865' ,width=10, justify='center')
+    entry2.place(relx=0.7, rely=0.22, anchor='center')
+
+    button2 = tk.Button(frame9, text="LƯU", command=luu_image, bg='#007865', fg='#fff',
+                        font=('Arial', 12, 'bold'), width=13, height=2, relief='flat', borderwidth=0)
+    button2.place(relx=0.5, rely=0.45, anchor='center')
+
+    # Thiết kế Frame 10
+    labelImg = tk.Label(frame10, image=photo)
+    labelImg.place(relwidth=1, relheight=1)
+
+    label1 = tk.Label(frame10, text="THÔNG TIN CHỈ SỐ", font=('Arial', 20, 'bold'), bg='#d0e7e5',
+                      fg='#007865')
+    label1.place(relx=0.5, rely=0.08, anchor='center')
+
+    label2 = tk.Label(frame10, text="Bước 1", font=('Arial', 20, 'bold'), bg='#CFE1E4', fg='#007865')
+    label2.place(relx=0.5, rely=0.15, anchor='center')
+
+    label1 = tk.Label(frame10, text="Kernel Size: ", font=('Arial', 15, 'bold'), bg='#D2D7E1', fg='#007865')
+    label1.place(relx=0.3, rely=0.22, anchor='center')
+    entry1 = tk.Entry(frame10,textvariable = kernel_size_var_cam, font=('Arial',15,'normal'), fg='#007865', width=10, justify='center')
+    entry1.place(relx=0.425, rely=0.22, anchor='center')
+
+    label2 = tk.Label(frame10, text="Sigma: ", font=('Arial', 15, 'bold'), bg='#D2D7E1', fg='#007865')
+    label2.place(relx=0.6, rely=0.22, anchor='center')
+    entry2 = tk.Entry(frame10,textvariable = sigma_var_cam, font=('Arial',15,'normal'), fg='#007865' ,width=10, justify='center')
+    entry2.place(relx=0.7, rely=0.22, anchor='center')
+
+    button2 = tk.Button(frame10, text="LƯU", command=luu_camera, bg='#007865', fg='#fff',
+                        font=('Arial', 12, 'bold'), width=13, height=2, relief='flat', borderwidth=0)
+    button2.place(relx=0.5, rely=0.45, anchor='center')
+
     show_frame(frame1)
     main.mainloop()
 
